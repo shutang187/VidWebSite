424877b317add5a85549748cbda0e09b323cf158
tangxianshu <3071633621@qq.com>
2023-10-05 19:10:07 +0800

    [add]新增minio配置和其工具类

--- bilibili/src/test/java/com/tangxs/bilibili/BilibiliApplicationTests.java
+++ bilibili/src/test/java/com/tangxs/bilibili/BilibiliApplicationTests.java
@@ -1,13 +1,19 @@
 package com.tangxs.bilibili;
 
+import com.tangxs.bilibili.util.MinioUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BilibiliApplicationTests {
 
+    @Autowired
+    private MinioUtil minioUtil;
     @Test
-    void contextLoads() {
+    void test() {
+        Boolean video = minioUtil.bucketExists("video");
+        System.out.println(video);
     }
 
 }
